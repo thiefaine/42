c1c79a0539b5b1b846d40335aac9202655ba235c
Matthias DUBRAY <mdubray@e1r3p3.42.fr>
2014-09-18 17:19:59 +0200

    Librairie fonctions C

---
+++ Lib/ft_lib.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lib.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdubray <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/09/10 01:16:34 by mdubray           #+#    #+#             */
+/*   Updated: 2014/09/10 01:18:43 by mdubray          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIB_H
+# define FT_LIB_H
+
+void	ft_putchar(char c);
+void	ft_putnbr(int nb);
+void	ft_putstr(char *str);
+void	ft_swap(int *a, int *b);
+int		ft_strlen(char *c);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
